73cb65d9683b7416abb5a1712fcf45e45ec51e29
Ivan Hossain <ivan2192011@gmail.com>
2020-02-27 08:16:35 +0800

    done

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,48 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
+  def top
+      @restaurants = Restaurant.where(stars: 5)
+    end
+
+  def index
+     @restaurants = Restaurant.all
+    end
+
+  def show
+    @reviews = @restaurant.reviews
+     @review = Review.new
+    end
+
+   def new
+      @restaurant = Restaurant.new
+    end
+
+   def edit
+    end
+
+  def create
+      @restaurant = Restaurant.new(restaurant_params)
+      @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    end
+
+  def update
+    @restaurant.update(restaurant_params)
+    redirect_to @restaurant
+  end
+
+  def destroy
+    @restaurant.destroy
+  end
+
+  private
+
+    def set_restaurant
+      @restaurant = Restaurant.find(params[:id])
+    end
+
+    def restaurant_params
+      params.require(:restaurant).permit(:name, :address, :category, :rating)
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+ {
+   name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:  'chinese',
+    phone_number: '4562211890'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:  'italian',
+    phone_number: '6842200942'
+  },
+  {
+    name: 'Jerk Chicken',
+    address: '67 Jamica Ave, NY-11342',
+    category: 'belgian',
+    phone_number: '2358899542'
+  },
+  {
+    name: 'Paco Deli',
+    address: '34 Whitestone RD',
+    category: 'french',
+    phone_number: '5683321789'
+  },
+  {
+    name:  'Punjab',
+    address:  '34 Canggu',
+    category: 'japanese',
+    phone_number: '235783264'
+    },
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
